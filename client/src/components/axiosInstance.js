75d293f6a4b0f080a04bda4e5ec55627a1f4b597
Illa Venkatesh <Venkateshilla1818@gmail.com>
2026-07-01 11:51:43 +0530

    Connect frontend to Railway API

--- client/src/components/axiosInstance.js
+++ client/src/components/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://sb-stocks-production.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
